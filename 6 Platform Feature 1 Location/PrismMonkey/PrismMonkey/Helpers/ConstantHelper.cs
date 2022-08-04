5375c7fcf6e09511380bf05dd186f1f30f1de728
Vulcan <vulcan.lee@gmail.com>
2022-08-04 11:15:51 +0800

    平台特性 Platform Feature 設計 - 取得 GPS 位置

---
+++ 6 Platform Feature 1 Location/PrismMonkey/PrismMonkey/Helpers/ConstantHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace PrismMonkey.Helpers
+{
+    /// <summary>
+    /// 管理該專案使用到的字串資源
+    /// </summary>
+    public static class ConstantHelper
+    {
+        #region 該應用程式中的頁面名稱字串
+        /// <summary>
+        /// 猴子集合物件清單的頁面名稱
+        /// </summary>
+        public static readonly string MonkeyListPage = nameof(MonkeyListPage);
+        /// <summary>
+        /// 猴子明細資訊的頁面名稱
+        /// </summary>
+        public static readonly string MonkeyDetailPage = nameof(MonkeyDetailPage);
+        #endregion
+
+        #region 頁面導航用到的參數鍵值
+        /// <summary>
+        /// 傳入猴子參數的鍵值名稱
+        /// </summary>
+        public static readonly string NavigationKeyMonkey = "Monkey";
+        #endregion
+    }
+}
---
+++ 6 Platform Feature 1 Location/PrismMonkey/PrismMonkey/MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿namespace PrismMonkey;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UsePrismApp<App>(PrismStartup.Configure)
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        #region 使用 Microsoft.Extensions.DependencyInjection 套件，註冊相關要用到的服務
+        builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+        builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+        #endregion
+
+        return builder.Build();
+    }
+}
