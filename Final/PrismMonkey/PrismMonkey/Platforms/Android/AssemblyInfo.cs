1b9f9c61044e7dd2e8189dee6be58d7a80935d6d
Vulcan <vulcan.lee@gmail.com>
2022-08-06 11:52:36 +0800

    完成的專案

---
+++ Final/PrismMonkey/PrismMonkey/Platforms/Android/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+
+#region 在這裡宣告 Android 系統中需要用到的權限 Permission
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+#endregion
---
+++ Final/PrismMonkey/PrismMonkey/Resources/Styles/MonkeyStyle.xaml.cs
@@ -0,0 +1,9 @@
+namespace PrismMonkey.Resources.Styles;
+
+public partial class MonkeyStyle : ResourceDictionary
+{
+	public MonkeyStyle()
+	{
+		InitializeComponent();
+	}
+}
